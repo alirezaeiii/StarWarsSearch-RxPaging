e71bd66b2fdfa7852e96101e008d9bc6a7aca6bc
Ali Rezaei <ali.rezaei09@gmail.com>
2021-03-16 08:52:29 +0330

    Modification

--- feature_search/src/main/java/com/android/sample/feature/search/ui/StarWarsItemViewHolder.kt
+++ feature_search/src/main/java/com/android/sample/feature/search/ui/StarWarsItemViewHolder.kt
@@ -1,11 +1,8 @@
 package com.android.sample.feature.search.ui
 
 import android.view.ViewGroup
-import androidx.databinding.DataBindingUtil
 import androidx.recyclerview.widget.RecyclerView
 import com.android.sample.commons.extension.layoutInflater
-import com.android.sample.core.model.Person
-import com.android.sample.feature.search.R
 import com.android.sample.feature.search.databinding.StarWarsItemBinding
 
 class StarWarsItemViewHolder(val binding: StarWarsItemBinding) :
@@ -13,8 +10,11 @@ class StarWarsItemViewHolder(val binding: StarWarsItemBinding) :
 
     companion object {
         fun create(parent: ViewGroup): StarWarsItemViewHolder {
-            val binding: StarWarsItemBinding =
-                StarWarsItemBinding.inflate(parent.context.layoutInflater)
+            val binding = StarWarsItemBinding.inflate(
+                parent.context.layoutInflater,
+                parent,
+                false
+            )
             return StarWarsItemViewHolder(binding)
         }
     }
