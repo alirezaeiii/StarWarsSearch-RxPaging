c186ce8d847a245e85de8a1678f9163b147e6143
Ali Rezaei <ali.rezaei09@gmail.com>
2021-04-04 22:08:58 +0430

    Modification

--- common/src/main/java/com/android/sample/common/paging/Listing.kt
+++ common/src/main/java/com/android/sample/common/paging/Listing.kt
@@ -8,7 +8,7 @@ import io.reactivex.Observable
  * Data class that is necessary for a UI to show a listing and interact w/ the rest of the system
  */
 data class Listing<T>(
-    // the LiveData of paged lists for the UI to observe
+    // the Observable of paged lists for the UI to observe
     val pagedList: Observable<PagedList<T>>,
     // represents the network request status to show to the user
     val networkState: LiveData<NetworkState>,
